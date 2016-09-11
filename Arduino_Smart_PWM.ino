babb3167d02fa2152d3c0fb8d391ceb32653cf7c
Bryan Smith <zarboz@gmail.com>
2016-09-11 12:03:32 -0500

    initial commit of pwm code with screen printout of information
    Signed-off-by: Bryan Smith <zarboz@gmail.com>

---
+++ Arduino_Smart_PWM.ino
@@ -0,0 +1,108 @@
+//pieced together and coded by the Zanderist(AWA)
+// display and smart pot added by zarboz
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define LOGO16_GLCD_HEIGHT 16 
+#define LOGO16_GLCD_WIDTH  16 
+
+int potpin = A3;
+int battpin = A2;
+int firepin = 7;
+
+int switchstate = 0;
+int mode = 0;
+int levelshutdown = 0;
+int readenable = 1;
+int potvalue = 0;
+int outputvalue = 0;
+int heatpwm = 0;
+int heatpwminc = -5;
+int samplepwm = 0;
+int outputpwm = 0;
+int voltagevalue = 0;
+int voutputvoltage = 0;
+
+
+float Rratio=0.4;
+float vout=0;
+float vin=0;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(firepin, INPUT);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+   display.display();
+   display.clearDisplay();
+}
+
+void loop () {
+
+  potvalue = analogRead(potpin);
+  outputvalue = map(potvalue, 0, 1023, 0, 255);
+  voltagevalue = analogRead(battpin);
+  vout = (voltagevalue*4.68)/1024.0;
+  vin = vout/Rratio;
+
+  switchstate = digitalRead(firepin);
+  if(readenable==1){
+  outputpwm=outputvalue;
+  if(vin>7.51){mode=2;}
+  if(6.99<=vin&&vin<=7.50){mode=1;}
+  if(vin<6.98){mode=0;}
+  }
+  
+  
+  if(switchstate==1){
+   readenable=0;
+    if(mode==2){
+      
+    bathighvoltage();}
+    
+   if(mode==1){
+    
+    batmediumvoltage();}
+  
+  
+   if(mode==0) {
+    
+    batlowvoltage(); }
+  }
+
+  
+  if(switchstate==0){
+    readenable=1;
+    samplepwm=1; 
+  }
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println(readenable);
+  display.setCursor(1,0);
+  display.println(potvalue);
+  display.setCursor(0,5);
+  display.println(outputvalue);
+  display.setCursor(1,5);
+  display.println(vin);
+}
+
+
+
+void bathighvoltage(){
+}
+/////////////////////////////\
+void batmediumvoltage(){
+}
+
+/////////////////////////////\
+void batlowvoltage(){
+}
+/////////////////////////////
