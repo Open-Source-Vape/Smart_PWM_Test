17cd2befd55b3bf195024458f47fd15da9940cc7
Bryan Smith <zarboz@gmail.com>
2016-09-28 18:00:56 -0500

    update components used
    Signed-off-by: Bryan Smith <zarboz@gmail.com>

--- NewPWM.ino
+++ NewPWM.ino
@@ -1,3 +1,15 @@
+/*
+ * Components used:
+ * https://www.adafruit.com/products/931
+ * https://www.sparkfun.com/products/9028
+ * https://www.sparkfun.com/products/8374
+ * https://www.sparkfun.com/products/13760
+ * https://www.sparkfun.com/products/97
+ * http://www.infineon.com/cms/en/product/power/power-mosfet/20v-300v-n-channel-power-mosfet/20v-30v-n-channel-power-mosfet/IRLS3813/productType.html?productType=5546d462533600a401533d4c1e1f77fa
+ * https://www.radioshack.com/products/radioshack-220-ohm-1-4w-5-carbon-film-resistor-pk-5
+ * 
+ */
+
 #include <PWM.h>
 #include <Wire.h>
 #include <SPI.h>
@@ -17,7 +29,7 @@ int uppin = 12;
 int downpin = 11;
 
 
-int32_t frequency = 500;
+int32_t frequency = 120;
 bool switchstate;
 bool switchstateup;
 bool switchstatedown;
