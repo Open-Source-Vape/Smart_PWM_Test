2e24a5eb9fc7ed702bd9d15e2c4670c7288f4e79
Bryan Smith <bryan.smith@intridea.com>
2017-08-30 14:40:32 -0600

    Update NewPWM.ino
    Add comments

--- NewPWM.ino
+++ NewPWM.ino
@@ -8,6 +8,12 @@
   http://www.infineon.com/cms/en/product/power/power-mosfet/20v-300v-n-channel-power-mosfet/20v-30v-n-channel-power-mosfet/IRLS3813/productType.html?productType=5546d462533600a401533d4c1e1f77fa
   https://www.radioshack.com/products/radioshack-220-ohm-1-4w-5-carbon-film-resistor-pk-5
   place 220 ohm resistor between MCU pin 7 and gate on mosfet
+  todo:
+  fixup userland editable PWM frequency and set limits for oscillation 
+  fixup locking mechanism
+  fixup display prettyness
+  write my own graphics lib with better fonts and smoothing
+  
 */
 
 #include <PWM.h>
@@ -802,4 +808,3 @@ void sleepnow() {
   sleep_mode();
 
 }
-
